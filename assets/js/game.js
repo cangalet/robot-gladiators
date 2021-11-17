00ed431091313badf7b5c6cdfa7db41ea80d42a7
Chris <cangalet@gmail.com>
2021-11-16 17:52:22 -0800

    Added randomization to fight order

--- assets/js/game.js
+++ assets/js/game.js
@@ -36,49 +36,59 @@ var fightOrSkip = function() {
 }
 
 var fight = function(enemy) {
-    while(playerInfo.health > 0 && enemy.health > 0) {
+    // keep track of who goes first
+    var isPlayerTurn = true;
 
-        if (fightOrSkip()) {
-            // if true, leave  fight by breaking loop
-            break;
-        }
+    if (Math.random() > 0.5) {
+        isPlayerTurn = false;
+    }
+    
+    while(playerInfo.health > 0 && enemy.health > 0) {    
+        if (isPlayerTurn) {
+            if (fightOrSkip()) {
+                // if true, leave  fight by breaking loop
+                break;
+            }
 
-        // generate random damage value based on player's attack power
-        var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
+            // generate random damage value based on player's attack power
+            var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
 
-        enemy.health = Math.max(0, enemy.health - damage);
+            enemy.health = Math.max(0, enemy.health - damage);
 
-        // Log a resulting message to the console so we know that it worked.
-        console.log (
-            playerInfo.name + " attacked " + enemy.name + ". " + enemy.name + " now has " + enemy.health + " health remaining"
-        );
+            // Log a resulting message to the console so we know that it worked.
+            console.log (
+                playerInfo.name + " attacked " + enemy.name + ". " + enemy.name + " now has " + enemy.health + " health remaining"
+            );
 
-        // check enemy's health
-        if (enemy.health <= 0) {
-            window.alert(enemy.name + " has died!");
-            playerInfo.money = playerInfo.money + 20;
-            break;
+            // check enemy's health
+            if (enemy.health <= 0) {
+                window.alert(enemy.name + " has died!");
+                playerInfo.money = playerInfo.money + 20;
+                break;
+            } else {
+                window.alert(enemy.name + " still has " + enemy.health + " health left.");
+            }
         } else {
-            window.alert(enemy.name + " still has " + enemy.health + " health left.");
+            // remove players's health by substracting the amount set in the enemy.attack variable
+            var damage = randomNumber(enemy.attack - 3, enemy.attack);
+
+            playerInfo.health = Math.max(0, playerInfo.health - damage);
+
+            // Log a resulting message to the console so we know that it worked.
+            console.log(
+                enemy.name + " attacked " + playerInfo.name + ". " + playerInfo.name + " now has " +playerInfo.health + " health remaining"
+            );
+
+            // check player's health
+            if (playerInfo.health <=0) {
+                window.alert(playerInfo.name + " has died!");
+                break;
+            } else {
+                window.alert(playerInfo.name + " still has " + playerInfo.health + " health left");
+            }
         }
-
-        // remove players's health by substracting the amount set in the enemy.attack variable
-        var damage = randomNumber(enemy.attack - 3, enemy.attack);
-
-        playerInfo.health = Math.max(0, playerInfo.health - damage);
-
-        // Log a resulting message to the console so we know that it worked.
-        console.log(
-            enemy.name + " attacked " + playerInfo.name + ". " + playerInfo.name + " now has " +playerInfo.health + " health remaining"
-        );
-
-        // check player's health
-        if (playerInfo.health <=0) {
-            window.alert(playerInfo.name + " has died!");
-            break;
-        } else {
-            window.alert(playerInfo.name + " still has " + playerInfo.health + " health left");
-        }     
+        // switch turn order for next round
+        isPlayerTurn = !isPlayerTurn;             
     }         
 };
 
